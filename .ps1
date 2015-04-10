f71ba4c1bc23b12cdd07782f95f2274323d7b03e
The-Abe <abevanderwielen@gmail.com>
2015-04-10 09:40:50 +0200

    Added comments and used trap to make input stand out from output

--- .ps1
+++ .ps1
@@ -1,10 +1,27 @@
+# Author: Abe van der Wielen
+# Date: 2015-04-10
+# Email: abevanderwielen@gmail.com
+# Website: https://github.com/the-abe
+# File: .ps1
+#
+# Source this file in .bashrc to get my fancy schmancy PS1 prompt.
+# Example:
+#   ~/bashrc
+#   $ (master +) echo "test"
+
+
+#COLOR Definitions
 BIYellow="\[\033[1;93m\]"
 BIPurple="\[\033[1;95m\]"
 BICyan="\[\033[1;96m\]"
+BIBlue="\[\033[1;34m\]"
 
+# Return just the git branch name
 function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
+
+# Return just the hg branch name
 function parse_hg_branch {
   hg branch 2>/dev/null
 }
@@ -14,6 +31,10 @@ function parse_git_changes {
     echo " +"
   fi
 }
+
+# Return a string based on the changes in hg status
+# + for modified files
+# ? for untracked files
 function parse_hg_changes {
   st=$(hg st 2>/dev/null)
   add=$(echo $st | grep -c "? app" | grep -v "\.orig$")
@@ -26,6 +47,8 @@ function parse_hg_changes {
     echo " +"
   fi
 }
+
+# Return a string for both the git and hg branches
 function parse_branches {
   branch=$(parse_git_branch)$(parse_hg_branch)
   if [ ! -z "$branch" ]; then
@@ -34,6 +57,8 @@ function parse_branches {
     echo ""
   fi
 }
+
+# Return a string for both the git and hg changes
 function parse_changes {
   changes=$(parse_git_changes)$(parse_hg_changes)
   if [ ! -z "$branch" ]; then
@@ -42,10 +67,19 @@ function parse_changes {
     echo ""
   fi
 }
+
+# Run this command every time a prompt shows up.
+# If we just define PS1 and the command output it only runs the first time.
 PROMPT_COMMAND=set_bash_prompt
 
+# Get the branch and changes string and put it in PS1
 function set_bash_prompt {
   branch=$(parse_branches)
   changes=$(parse_changes)
-  PS1="$BICyan\w\n$BIPurple\$$BIYellow$branch$changes\[$(tput sgr0)\] "
+  # Path \n $/# git/hg
+  #PS1="$BICyan\w\n$BIPurple\$$BIYellow$branch$changes\[$(tput sgr0)\] "
+  PS1="$BICyan\w\n$BIPurple\$$BIYellow$branch$changes$BIBlue "
 }
+
+trap '[[ -t 1 ]] && tput sgr0' DEBUG
+# vim:ft=sh
